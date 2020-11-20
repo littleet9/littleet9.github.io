67884c014718ea83cc76200a3c09bebf51166508
littleet9 <littleet@appstate.edu>
2020-11-20 18:27:15 -0500

    202020 11 202020 is now working

--- scripts/20202011202020.js
+++ scripts/20202011202020.js
@@ -1,8 +1,37 @@
 function main() {
     $('section').hide();
     $('section').fadeIn(500);
+    updateTime();
 }
 main();
+
+
+
+function updateTime(){
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+	let hour = today.getHours();
+	let mins = today.getMinutes();
+    let secs = today.getSeconds();
+	if(hour < 10)
+		hour = "0" + hour;
+	if(mins < 10)
+		mins = "0" + mins;
+	if(secs < 10)
+		secs = "0" + secs;
+        
+        
+    let h1 = document.getElementById("timeHeader");
+    h1.innerHTML = hour + "" + mins + "" + secs + "" + month  + "" + day + "" + year;
+
+	let h2 = document.getElementById("theDate");
+    h2.innerHTML = hour + " : " + mins + " : " + secs + "<br />" + month + "/" + day + "/" + year;
+	
+	setTimeout("updateTime();",1000);
+}
+
 
 
 
