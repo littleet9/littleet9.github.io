9b1020594478bff69a3f63fdddbcadc4ff19504f
littleet9 <littleet@appstate.edu>
2020-11-20 20:05:29 -0500

    Changed some css

--- scripts/20202011202020.js
+++ scripts/20202011202020.js
@@ -1,33 +1,63 @@
 function main() {
     $('section').hide();
     $('section').fadeIn(500);
-    updateTime();
+    initTime();
+    
+    
 }
 main();
 
+function initTime(){
+   // $('.funNumber').css('display', 'inline');
+   // $('.funNumber').css('float', 'left');
+    $('.funNumber').on('mouseover', function() {
+            $(this).css('color', "white");
+            $(this).css('cursor', 'default');
+            $(this).animate({
+                fontSize: 160
+            }, 100, 'swing', ''); 
+    }).on('mouseout', function() {
+            $(this).css('color', "#FFC857");
+            $(this).animate({
+                fontSize: 150
+            }, 100, 'swing', ''); 
+    });
 
+
+    updateTime();
+}
 
 function updateTime(){
     let today = new Date();
-    let day = today.getDate();
-    let month = today.getMonth() + 1;
-    let year = today.getFullYear();
-	let hour = today.getHours();
-	let mins = today.getMinutes();
+    //-----------------------
+    let hour = today.getHours();
+    let hourLI = document.getElementById('hourLI');
+    if(hour < 10)
+    hour = "0" + hour;
+    hourLI.innerHTML = hour;
+    let mins = today.getMinutes();
+    let minLI = document.getElementById('minLI');
+    if(mins < 10)
+    mins = "0" + mins;
+    minLI.innerHTML = mins;
     let secs = today.getSeconds();
-	if(hour < 10)
-		hour = "0" + hour;
-	if(mins < 10)
-		mins = "0" + mins;
-	if(secs < 10)
-		secs = "0" + secs;
-        
-        
-    let h1 = document.getElementById("timeHeader");
-    h1.innerHTML = hour + "" + mins + "" + secs + "" + month  + "" + day + "" + year;
+    let secLI = document.getElementById('secLI');
+    if(secs < 10)
+    secs = "0" + secs;
+    secLI.innerHTML = secs;
 
-	let h2 = document.getElementById("theDate");
-    h2.innerHTML = hour + " : " + mins + " : " + secs + "<br />" + month + "/" + day + "/" + year;
+    let month = today.getMonth() + 1;
+    let monthLI = document.getElementById('monthLI');
+    monthLI.innerHTML = month;
+    let day = today.getDate();
+    let dayLI = document.getElementById('dayLI');
+    dayLI.innerHTML = day;
+    let year = today.getFullYear();
+    let yearLI = document.getElementById('yearLI');
+    yearLI.innerHTML = year;
+
+	//let h2 = document.getElementById("theDate");
+    //h2.innerHTML = hour + " : " + mins + " : " + secs + "<br />" + month + "/" + day + "/" + year;
 	
 	setTimeout("updateTime();",1000);
 }
