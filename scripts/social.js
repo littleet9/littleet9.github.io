bcdddc26b7e9dcdafe171ca421aa93c5d0e833bf
littleet9 <littleet@appstate.edu>
2020-11-26 15:06:30 -0500

    Added new background to social page

--- scripts/social.js
+++ scripts/social.js
@@ -1,3 +1,5 @@
+let ccfCounter = 0;
+let ccfPhotos = 3;
 let ytCounter = 0; //Counter for which video is currently displayed
 let videosIFrames = ["https://www.youtube.com/embed/La7ve-SfxK0",
 					"https://www.youtube.com/embed/jCfda-MPo1k",
@@ -7,24 +9,53 @@ let videosIFrames = ["https://www.youtube.com/embed/La7ve-SfxK0",
 					"https://www.youtube.com/embed/zO6GOGCgt3g"];
 
 function main() {
+	initSlideButtons();
+	initCCFPhotos();
 	initYTVideos();
 }
 main();
 
-function initYTVideos(){
+function initSlideButtons(){
 	checkArrayBounds();
-	$('.ytButtons').on('mouseover', function(){
-		$(this).css('cursor', 'pointer');
+	$('.slideButtons').on('mouseover', function(){
+		$(this).css('cursor', 'pointer'); 
 	}).on('mouseout', function(){
 		$(this).css('cursor', 'default');
-	}).on('click', function(){
-		
 	});
+}
 
+function initCCFPhotos(){
+	$('#ccfprevButton').on('click', function(){
+		if(ccfCounter > 0)
+			ccfCounter--;
+		$('#ccfphoto1').attr('src', "media/CCFphotos/" + (ccfCounter * 3 + 1) + ".jpg");
+		$('#ccfphoto2').attr('src', "media/CCFphotos/" + (ccfCounter * 3 + 2) + ".jpg");
+		$('#ccfphoto3').attr('src', "media/CCFphotos/" + (ccfCounter * 3 + 3) + ".jpg");
+		checkArrayBounds();
+	}).on('mouseover', function(){
+		$(this).children().attr('src', 'media/prevButtonOver.png');
+	}).on('mouseout', function(){
+		$(this).children().attr('src', 'media/prevButton.png');
+	});
+	
+	$('#ccfnextButton').on('click', function(){
+		if(ccfCounter < ccfPhotos-1)
+			ccfCounter++;
+		$('#ccfphoto1').attr('src', "media/CCFphotos/" + (ccfCounter * 3 + 1) + ".jpg");
+		$('#ccfphoto2').attr('src', "media/CCFphotos/" + (ccfCounter * 3 + 2) + ".jpg");
+		$('#ccfphoto3').attr('src', "media/CCFphotos/" + (ccfCounter * 3 + 3) + ".jpg");
+		checkArrayBounds();
+	}).on('mouseover', function(){
+		$(this).children().attr('src', 'media/nextButtonOver.png');
+	}).on('mouseout', function(){
+		$(this).children().attr('src', 'media/nextButton.png');
+	});
+}
+
+function initYTVideos(){
 	$('#ytprevButton').on('click', function(){
 		if(ytCounter > 0)
 			ytCounter--;
-		console.log(ytCounter);
 		$('iframe').fadeOut(350);
 		$('iframe').attr('src', videosIFrames[ytCounter]);
 		$('iframe').fadeIn(350);
@@ -38,7 +69,6 @@ function initYTVideos(){
 	$('#ytnextButton').on('click', function(){
 		if(ytCounter < videosIFrames.length-1)
 			ytCounter++;
-		console.log(ytCounter);
 		$('iframe').fadeOut(350);
 		$('iframe').attr('src', videosIFrames[ytCounter]);
 		$('iframe').fadeIn(350);
@@ -51,6 +81,20 @@ function initYTVideos(){
 }
 
 function checkArrayBounds(){
+	//CCF Photos
+	if(ccfCounter == 0){
+		$('#ccfprevButton').css('visibility', 'hidden');
+		$('#ccfnextButton').css('visibility', 'visible');
+	}
+	else if(ccfCounter == ccfPhotos - 1){
+		$('#ccfprevButton').css('visibility', 'visible');
+		$('#ccfnextButton').css('visibility', 'hidden');
+	}
+	else{
+		$('#ccfprevButton').css('visibility', 'visible');
+		$('#ccfnextButton').css('visibility', 'visible');
+	}
+	//Youtube
 	if(ytCounter == 0){
 		$('#ytprevButton').css('visibility', 'hidden');
 		$('#ytnextButton').css('visibility', 'visible');
